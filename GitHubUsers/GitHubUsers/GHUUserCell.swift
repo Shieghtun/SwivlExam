981f1417eff06704e697c6cac9a7febdc000992f
Shieghtun <skovalenko@stanfy.com.ua>
2015-10-24 22:18:57 +0300

    Initial project, base autolayout and navigation, cocoapods enabled

---
+++ GitHubUsers/GitHubUsers/GHUUserCell.swift
@@ -0,0 +1,13 @@
+//
+//  GHUUserCell.swift
+//  GitHubUsers
+//
+//  Created by admin on 10/24/15.
+//  Copyright © 2015 Shieghtun. All rights reserved.
+//
+
+import UIKit
+
+class GHUUserCell: UITableViewCell {
+
+}
